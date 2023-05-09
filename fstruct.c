06198a74af4f36d9ef44c65f839c8a978317213d
RM92023 <robinjara20@gmail.com>
2023-05-09 16:08:13 -0500

    Files update in betty-styles

--- fstruct.c
+++ fstruct.c
@@ -1,61 +1,68 @@
 #include "monty.h"
+/**
+ * monty_interpreter - Interpreter for themonty functions
+ *
+ * @filename: The name of the file to be modified
+ *
+ * Return: The modified file name for the function
+ */
 
 int monty_interpreter(char *filename)
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    stack_t *stack = NULL;
-    unsigned int line_number = 1;
+FILE *file;
+char *line = NULL;
+size_t len = 0;
+ssize_t read;
+stack_t *stack = NULL;
+unsigned int line_number = 1;
 
-    file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        return (-1);
-    }
+file = fopen(filename, "r");
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+return (-1);
+}
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        /* Verificar si la línea está en blanco o solo tiene espacios*/
-        if (strspn(line, " \t\n") == (size_t)read)
-        {
-            line_number++;
-            continue;
-        }
-        char *token = strtok(line, " \n\t");
-        if (token != NULL && strcmp(token, "push") == 0)
-        {
-            token = strtok(NULL, " \n\t");
-            if (token != NULL &&
-                ((isdigit(*token) || (*token == '-' && isdigit(*(token + 1)))) &&
-                strspn(token, "-0123456789") == strlen(token))
-            )
-                push(&stack, atoi(token));
-            else
-            {
-                fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                free(line);
-                fclose(file);
-                free_stack(&stack);
-                return (-1);
-            }
-        }
-        else if (token != NULL && strcmp(token, "pall") == 0)
-            pall(stack);
-        else
-        {
-            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
-            free(line);
-            fclose(file);
-            free_stack(&stack);
-            return (-1);
-        }
-        line_number++;
-    }
-    free(line);
-    fclose(file);
-    free_stack(&stack);
-    return (0);
+while ((read = getline(&line, &len, file)) != -1)
+{
+/* Verificar si la línea está en blanco o solo tiene espacios*/
+if (strspn(line, " \t\n") == (size_t)read)
+{
+line_number++;
+continue;
+}
+char *token = strtok(line, " \n\t");
+if (token != NULL && strcmp(token, "push") == 0)
+{
+token = strtok(NULL, " \n\t");
+if (token != NULL &&
+((isdigit(*token) || (*token == '-' && isdigit(*(token + 1)))) &&
+strspn(token, "-0123456789") == strlen(token))
+)
+push(&stack, atoi(token));
+else
+{
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+free(line);
+fclose(file);
+free_stack(&stack);
+return (-1);
+}
+}
+else if (token != NULL && strcmp(token, "pall") == 0)
+pall(stack);
+else
+{
+fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
+free(line);
+fclose(file);
+free_stack(&stack);
+return (-1);
+}
+line_number++;
+}
+free(line);
+fclose(file);
+free_stack(&stack);
+return (0);
 }
--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include "monty.h"
 
+/**
+ * main - main program
+ *
+ * @argc: number of arguments
+ * @argv: command line arguments
+ *
+ * Return: 0 on success
+ */
+
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
 
-    if (monty_interpreter(argv[1]) == -1)
-        exit(EXIT_FAILURE);
-
-    exit(EXIT_SUCCESS);
+if (monty_interpreter(argv[1]) == -1)
+exit(EXIT_FAILURE);
+exit(EXIT_SUCCESS);
 }
