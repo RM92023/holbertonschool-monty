77ce7bd9b75ff1507b57664de22d9d5c68f60898
RM92023 <robinjara20@gmail.com>
2023-05-09 15:39:00 -0500

    updating main.c because this function has more 40 lines in code

--- main.c
+++ main.c
@@ -11,62 +11,41 @@
 int main(int argc, char **argv)
 {
 FILE *file;
-char *line = NULL;
+char *line = NULL, *token;
 size_t len = 0;
 ssize_t read;
 stack_t *stack = NULL;
 unsigned int line_number = 1;
-
 if (argc != 2)
-{
-fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE);
-}
+return (fprintf(stderr, "USAGE: monty file\n"), EXIT_FAILURE);
 file = fopen(argv[1], "r");
 if (file == NULL)
-{
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-exit(EXIT_FAILURE);
-}
+return (fprintf(stderr, "Error: Can't open file %s\n", argv[1]), EXIT_FAILURE);
 while ((read = getline(&line, &len, file)) != -1)
 {
-/* Verificar si la línea está en blanco o solo tiene espacios*/
 if (strspn(line, " \t\n") == (size_t)read)
-{
-line_number++;
 continue;
-}
-char *token = strtok(line, " \n\t");
+token = strtok(line, " \n\t");
 if (token != NULL && strcmp(token, "push") == 0)
 {
 token = strtok(NULL, " \n\t");
-if (token != NULL &&
-((isdigit(*token) || (*token == '-' && isdigit(*(token + 1)))) &&
-strspn(token, "-0123456789") == strlen(token)))
+if (token != NULL && ((isdigit(*token) ||
+(*token == '-' && isdigit(*(token + 1))))
+&& strspn(token, "-0123456789") == strlen(token)))
 push(&stack, atoi(token));
 else
-{
-fprintf(stderr, "L%u: usage: push integer\n", line_number);
-free(line);
-fclose(file);
-free_stack(&stack);
-exit(EXIT_FAILURE);
-}
+return (fprintf(stderr, "L%u: usage: push integer\n", line_number),
+free(line), fclose(file),
+free_stack(&stack), EXIT_FAILURE);
 }
 else if (token != NULL && strcmp(token, "pall") == 0)
 pall(stack);
 else
-{
-fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
-free(line);
-fclose(file);
-free_stack(&stack);
-exit(EXIT_FAILURE);
-}
+return (fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token),
+free(line),
+fclose(file), free_stack(&stack), EXIT_FAILURE);
 line_number++;
 }
-free(line);
-fclose(file);
-free_stack(&stack);
+free(line), fclose(file), free_stack(&stack);
 return (EXIT_SUCCESS);
 }
